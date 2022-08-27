6d3b645d0db96788b0002b3aa004c4540c9ed222
francisallin <lamtszto1997@gmail.com>
2022-08-27 00:16:43 -0400

    Complete reverString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
 
+const reverseString = function(string) {
+    //=prompt ("Give me the string you want me to reverse for you",'');
+    let newString='';
+    for (let n=string.length-1; n>=0; n--){
+        newString+=string.charAt(n);
+    }
+    return newString
 };
 
 // Do not edit below this line
