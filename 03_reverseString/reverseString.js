47b6df360f8d71f29cd98453b3f37f0cc9aff77d
francisallin <lamtszto1997@gmail.com>
2022-08-27 09:11:02 -0400

    Remove uncessary codes

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,5 @@
 
 const reverseString = function(string) {
-    //=prompt ("Give me the string you want me to reverse for you",'');
     let newString='';
     for (let n=string.length-1; n>=0; n--){
         newString+=string.charAt(n);
